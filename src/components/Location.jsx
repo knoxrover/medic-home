fedac23cdc394e441fc47944fbdaf582c8bdcbf1
Shubhamrawat5 <sr856161@gmail.com>
2021-08-15 02:30:31 +0530

    search function implement

--- src/components/Location.jsx
+++ src/components/Location.jsx
@@ -9,7 +9,7 @@ function Location() {
             className="map-img"
             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3449.9452350170786!2d78.77044371462267!3d30.15298172039295!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3909a46c8c2f6857%3A0x9183e1bbd486352c!2sAgency%20Chowk%2C%20Pauri%2C%20Uttarakhand%20246001!5e0!3m2!1sen!2sin!4v1628357083516!5m2!1sen!2sin"
             style={{ border: 0 }}
-            allowfullscreen=""
+            allowFullScreen=""
             loading="lazy"
           ></iframe>
         </span>
--- src/views/Products.jsx
+++ src/views/Products.jsx
@@ -5,12 +5,12 @@ class Products extends React.Component {
   state = {
     products: [
       {
-        title: "Supradyn Multivitamin Tablet",
+        title: "Supradyn Multivitamin Talet",
         price: 33.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/165555/supradyn-multivitamin-tablets-strip-of-15-s-2-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title: "Becozym C Forte",
+        title: "Becozym Forte",
         price: 323.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
@@ -20,13 +20,32 @@ class Products extends React.Component {
         img: "https://cdn01.pharmeasy.in/dam/products_otc/270620/digene-acidity-gas-relief-tablets-15s-mint-flavour-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title: "Supradyn Multivitamin Tablet",
+        title: "Supradyn ltivitamin Tablet",
         price: 33.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/165555/supradyn-multivitamin-tablets-strip-of-15-s-2-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title:
-          "Depura 60k Nano VitDVitamin Drops Depura 60k Nano Vit D Vitamin",
+        title: "Depa 60k Nano VitDVitamin Drops Depura 60k Nano Vit D Vitamin",
+        price: 323.93,
+        img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
+      },
+      {
+        title: "Becozym C Fote",
+        price: 323.93,
+        img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
+      },
+      {
+        title: "Depura 60k Nano Vt D Vitamin Drops",
+        price: 53.93,
+        img: "https://cdn01.pharmeasy.in/dam/products_otc/270620/digene-acidity-gas-relief-tablets-15s-mint-flavour-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
+      },
+      {
+        title: "Supradyn Multitamin Talet",
+        price: 33.93,
+        img: "https://cdn01.pharmeasy.in/dam/products_otc/165555/supradyn-multivitamin-tablets-strip-of-15-s-2-1627613618.jpg?dim=224x0&dpr=1&q=100",
+      },
+      {
+        title: "Depura 60k Nano VitDVitami Drops Depura 60k Nano Vit D Vitamin",
         price: 323.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
@@ -36,54 +55,71 @@ class Products extends React.Component {
         img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title: "Depura 60k Nano Vit D Vitamin Drops",
+        title: "Depura 60k Nano Vit D Vitamin Drop",
         price: 53.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/270620/digene-acidity-gas-relief-tablets-15s-mint-flavour-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title: "Supradyn Multivitamin Tablet",
+        title: "Supradyn Multivitami Tablet",
         price: 33.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/165555/supradyn-multivitamin-tablets-strip-of-15-s-2-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
       {
-        title:
-          "Depura 60k Nano VitDVitamin Drops Depura 60k Nano Vit D Vitamin",
-        price: 323.93,
-        img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
-      },
-      {
-        title: "Becozym C Forte",
-        price: 323.93,
-        img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
-      },
-      {
-        title: "Depura 60k Nano Vit D Vitamin Drops",
-        price: 53.93,
-        img: "https://cdn01.pharmeasy.in/dam/products_otc/270620/digene-acidity-gas-relief-tablets-15s-mint-flavour-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
-      },
-      {
-        title: "Supradyn Multivitamin Tablet",
-        price: 33.93,
-        img: "https://cdn01.pharmeasy.in/dam/products_otc/165555/supradyn-multivitamin-tablets-strip-of-15-s-2-1627613618.jpg?dim=224x0&dpr=1&q=100",
-      },
-      {
-        title:
-          "Depura 60k Nano VitDVitamin Drops Depura 60k Nano Vit D Vitamin",
+        title: "Depura 60k N VitDVitamin Drops Depura 60k Nano Vit D Vitamin",
         price: 323.93,
         img: "https://cdn01.pharmeasy.in/dam/products_otc/022290/becozym-c-forte-vitamin-b-complex-biotin-vitamin-c-strip-of-15-tablets-1-1627613618.jpg?dim=224x0&dpr=1&q=100",
       },
     ],
+    search: "",
+    searchResult: [],
   };
+  searchHandler = (e) => {
+    // console.log(e.target.value);
+    let txt = e.target.value;
+
+    let res = [];
+    this.state.products.forEach((item) => {
+      let name = item.title.toLowerCase();
+      let txtLower = txt.toLowerCase();
+      if (name.includes(txtLower)) res.push(item);
+    });
+
+    this.setState({
+      ...this.state,
+      search: txt,
+      searchResult: res,
+    });
+  };
+
   render() {
+    let search = this.state.search;
+    let products = this.state.products;
+    let searchResult = this.state.searchResult;
     return (
       <section className="products-section">
-        <input className="search" type="text" placeholder="search.." />
-        <h2 className="products-section-heading">Products available</h2>
+        <input
+          className="search"
+          type="text"
+          placeholder="search.."
+          value={search}
+          onChange={(e) => {
+            this.searchHandler(e);
+          }}
+        />
+        <h2 className="products-section-heading">
+          {search === "" ? "Products available" : `Search result for ${search}`}
+        </h2>
 
         <div className="all-products">
-          {this.state.products.map((item) => {
-            return <Product item={item} />;
-          })}
+          {search === ""
+            ? products.map((item, index) => {
+                return <Product item={item} key={index} />;
+              })
+            : searchResult.length
+            ? searchResult.map((item, index) => {
+                return <Product item={item} key={index} />;
+              })
+            : "no result... "}
         </div>
       </section>
     );
