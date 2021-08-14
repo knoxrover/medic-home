dff7057202b97ae5a97f0799a7e21beac436f61a
Shubhamrawat5 <sr856161@gmail.com>
2021-08-14 16:59:49 +0530

    fix hamburger

--- script.js
+++ script.js
@@ -3,6 +3,12 @@ let navUl = document.querySelector(".nav-ul");
 hamMenu.addEventListener("click", toggleHamMenu);
 
 function toggleHamMenu() {
+  //   if (navUl.classList.contains("show")) {
+  //     navUl.classList.remove("show");
+  //     navUl.classList.add("hide");
+  //   } else {
+  //     navUl.classList.remove("hide");
+  //     navUl.classList.add("show");
+  //   }
   navUl.classList.toggle("show");
-  navUl.classList.toggle("hide");
 }
